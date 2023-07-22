47ef448bd0626726ea9164a7dbcf3f9ffec9e2de
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-07-22 22:55:24 +0300

    updPath

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from 'components/App';
 import { GlobalStyle } from 'GlobalStyle';
 import { BrowserRouter } from 'react-router-dom';
+// import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
