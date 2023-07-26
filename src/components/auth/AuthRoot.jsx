56ece585f6c053944f3e80543a71251504ce81ed
Inna Mykytiuk <innka-pinnka@ukr.net>
2023-07-26 21:45:14 +0300

    upd

--- src/components/auth/AuthRoot.jsx
+++ src/components/auth/AuthRoot.jsx
@@ -2,13 +2,29 @@ import React from 'react'
 import { useLocation } from 'react-router-dom'
 import LogIn from './login/LogIn'
 import Register from './register/Register'
+import { Root } from './AuthRoot.styled'
+import { Box } from '@mui/material'
 
 const AuthRoot = () => {
-  const location = useLocation()
+  const location = useLocation();
+
   return (
-    <>
-    {location.pathname === '/login' ? <LogIn/> : location.pathname === '/register' ? <Register/> : null}
-    </>
+    <Root>
+      <div>
+        <Box
+          display='flex'
+          justifyContent='center'
+          alignItems='center'
+          flexDirection='column'
+          maxWidth={640}
+          margin='auto'
+          padding={5}
+          borderRadius={5}
+        >
+        {(location.pathname === '/login' ? <LogIn/> : location.pathname === '/register' ? <Register/> : null)}
+        </Box>
+      </div>
+    </Root>
   )
 }
 
---
+++ src/components/auth/AuthRoot.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Root = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+
+`
