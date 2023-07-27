ce8eb80f8130df40d9f0d69f558864609cae09b0
Inna-Mykytiuk <innka-pinnka@ukr.net>
2023-07-27 18:31:52 +0000

    Deploying to gh-pages from @ Inna-Mykytiuk/react-practice@d5972d6a3505b8ecd3fbb509cf604a882fe16747 🚀

--- static/js/415.e5dc5153.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{5415:function(e,r,t){t.r(r);t(2791);var c=t(2634),n=t(184);r.default=function(){return(0,n.jsx)(c.$0,{children:(0,n.jsx)(c.W2,{children:(0,n.jsx)(c.Dx,{children:"Home page"})})})}}}]);
-//# sourceMappingURL=415.e5dc5153.chunk.js.map
